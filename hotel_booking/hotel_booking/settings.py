1bae0b5fc26381e6243228603daf60f0437e1b8e
meklitm7 <meklitmulugeta71@gmail.com>
2026-07-13 04:36:53 +0300

    Configure PostgreSQL for Render deployment

--- hotel_booking/hotel_booking/settings.py
+++ hotel_booking/hotel_booking/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
- 
+import dj_database_url 
 from pathlib import Path
 import os
 from dotenv import load_dotenv
@@ -87,15 +87,13 @@ WSGI_APPLICATION = 'hotel_booking.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": os.getenv("DB_NAME"),
-        "USER": os.getenv("DB_USER"),
-        "PASSWORD": os.getenv("DB_PASSWORD"),
-        "HOST": os.getenv("DB_HOST"),
-        "PORT": os.getenv("DB_PORT"),
-    }
+    "default": dj_database_url.config(
+        default=os.getenv("DATABASE_URL"),
+        conn_max_age=600,
+    )
 }
 
 
